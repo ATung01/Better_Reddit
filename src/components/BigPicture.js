1cc6efafd8bdbda0e72573b7e6153da4edb22040
adames <adameshh@gmail.com>
2017-08-22 14:43:44 -0400

    added splash page

--- src/components/BigPicture.js
+++ src/components/BigPicture.js
@@ -2,9 +2,7 @@ import React from 'react'
 import { Image } from 'semantic-ui-react'
 
 const BigPicture = (props) => {
-  const handleEmpty = () => props.selected ? props.selected['url'] : 'http://icoconvert.com/images/noimage2.png'
-
-  return <Image src={ handleEmpty() } size="big" centered />
+  return <Image src={ props.selected['url'] } size="big" centered />
 }
 
 export default BigPicture;
--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -1,20 +1,33 @@
 import React from 'react'
-import { Grid } from 'semantic-ui-react'
+import { Grid, Segment } from 'semantic-ui-react'
 import BigPicture from './BigPicture'
 import Info from './Info'
 
 export default class Viewer extends React.Component {
 
+  splashIfNoneSelected = (selected) => {
+    if (this.props.selected.length === 0) {
+      return <p>Welcome to Better Reddit</p>
+    }
+    else {
+      return (
+        <Grid>
+          <Grid.Column width={10}>
+            <BigPicture selected={selected[0]} />
+          </Grid.Column>
+          <Grid.Column width={5}>
+            <Info selected={selected[0]}/>
+          </Grid.Column>
+        </Grid>
+      )
+    }
+  }
+
   render(){
     return(
-      <Grid>
-        <Grid.Column width={10}>
-          <BigPicture selected={this.props.selected[0]} />
-        </Grid.Column>
-        <Grid.Column width={5}>
-          <Info selected={this.props.selected[0]}/>
-        </Grid.Column>
-      </Grid>
+      <Segment>
+        {this.splashIfNoneSelected(this.props.selected)}
+      </Segment>
     )
   }
 }
