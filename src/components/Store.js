31a3bdec1998e5dfb19823d1cc5f57f7ead8b439
adames <adameshh@gmail.com>
2017-08-23 17:39:16 -0400

    finished store

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,26 +1,51 @@
 import React from 'react'
 import Slider from 'react-slick'
-import { Segment } from 'semantic-ui-react'
+import { Segment, Image } from 'semantic-ui-react'
 
 export default class Store extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      store: []
+    }
+  }
+
+  getStore = () => {
+    return fetch("http://localhost:8080/posts/store")
+      .then(res => res.json())
+      .then(res => this.setState({
+        store: res
+      })
+    )
+  }
+
+  componentDidMount() {
+    this.getStore()
+  }
+
+  handleEmpty = () => {
+    if (this.state.store.length !== 0){
+      return this.state.store.map(p => {
+        return <div key={p.post_id} ><Image src={p.thumbnail} /></div>
+      })
+    }
+    else {
+      return <p>Store Empty</p>
+    }
+  }
 
   render() {
     let settings = {
       dots: true,
+      arrows: true,
       infinite: true,
-      speed: 500,
-      slidesToShow: 1,
+      slidesToShow: 3,
       slidesToScroll: 1
     };
     return (
       <Segment >
         <Slider {...settings}>
-          <div><h3>1</h3></div>
-          <div><h3>2</h3></div>
-          <div><h3>3</h3></div>
-          <div><h3>4</h3></div>
-          <div><h3>5</h3></div>
-          <div><h3>6</h3></div>
+          {this.handleEmpty()}
         </Slider>
       </Segment >
     );
